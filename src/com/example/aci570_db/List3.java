7db8090301ed99dfdfc8f15ab05b2d635b36743c
root <root@localhost.localdomain>
2013-12-15 21:25:07 -0300

    Examen aci810

---
+++ src/com/example/aci570_db/List3.java
@@ -0,0 +1,269 @@
+package com.example.aci570_db;
+
+
+
+
+
+import java.util.List;
+
+import com.example.aci570_db.db.MyAppDataSource;
+import com.example.aci570_db.db.MyAppDataSourceList3;
+import com.example.aci570_db.listeners.ListViewItemClickListener2;
+
+import com.example.aci570_db.model.Molist3;
+import com.example.aci570_db.model.Person;
+
+import android.os.Bundle;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+
+
+
+public class List3 extends ListActivity {
+
+	
+	public static final int REQUEST_CODE_ADD_ITEMS = 1;
+    public static final int REQUEST_CODE_UPDATE_ITEMS = 2;
+    
+    public static final String EXTRA_ITEMS = "e_items";
+    public static final String EXTRA_REMOVE_ITEMS = "e_remove_items";
+
+    private MyAppDataSourceList3 dslist3;
+    
+
+    private MyAppDataSource ds;
+    private Molist3 ItemsToUpdate;
+	private Person personToUpdate;
+	
+	
+    
+    
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_list3);
+                
+            setupActionBar();   
+            
+            dslist3 = new MyAppDataSourceList3(this);
+			dslist3.open();
+		
+		List<Molist3> values2 = dslist3.getItem();
+		
+		// use the SimpleCursorAdapter to show the elements in a ListView
+		ArrayAdapter<Molist3> adapter2 = new ArrayAdapter<Molist3>(
+		             this,
+		             android.R.layout.simple_list_item_1,
+		             values2
+		     );
+		
+		this.setListAdapter(adapter2);
+		
+		
+		
+		ListView lv2 = (ListView) this.findViewById(android.R.id.list);
+		lv2.setOnItemClickListener(new ListViewItemClickListener2(this));
+		
+		ds = new MyAppDataSource(this);
+	    ds.open();
+
+
+    
+    Intent i = this.getIntent();
+
+ if(i.hasExtra(Lista1.EXTRA_PERSON))
+ 	{
+	 Person p = (Person) i.getSerializableExtra(Lista1.EXTRA_PERSON);
+     
+	 		this.setTitle("shopping list  " + p.getFirstName()  );
+ 			
+ 			
+			
+			Button deleteButton = (Button) this.findViewById(R.id.Delete);
+			deleteButton.setVisibility(Button.VISIBLE);
+            
+			
+			this.personToUpdate = p;
+
+			            
+     }
+     else
+     {
+ 			             
+    	 this.personToUpdate = null;
+             
+}
+}
+
+	private void setupActionBar() {
+
+    getActionBar().setDisplayHomeAsUpEnabled(true);
+
+    
+
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+    
+    
+    
+    @Override
+    protected void onActivityResult(int requestCode2, int resultCode2, Intent data2) {
+            super.onActivityResult(requestCode2, resultCode2, data2);
+            
+            if(data2.hasExtra(EXTRA_ITEMS))
+            {
+                    List<Molist3> values2 = dslist3.getItem();
+            
+             // use the SimpleCursorAdapter to show the elements in a ListView
+             ArrayAdapter<Molist3> adapter2 = new ArrayAdapter<Molist3>(
+                             this,
+                             android.R.layout.simple_list_item_1,
+                             values2
+                     );
+            
+             ListView lv2 = (ListView) this.findViewById(android.R.id.list);
+             lv2.setAdapter(adapter2);
+            
+                    if(requestCode2 == REQUEST_CODE_ADD_ITEMS) {
+                            // do something here when a person is added
+                    }
+                    else if(requestCode2 == REQUEST_CODE_UPDATE_ITEMS)
+                    {
+                            Boolean remove2 = data2.getBooleanExtra(EXTRA_REMOVE_ITEMS, false);
+                            
+                            if(remove2) {
+                                    // do something here when a person is removed
+                            }
+                            else {
+                                    // do something here when a person is updated
+                            }
+                    }
+                    adapter2.notifyDataSetChanged();
+            }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.lista2, menu);
+            return true;
+    }
+    
+    public void onAddItemClick3(View view) {
+            Intent i = new Intent(this, List3Item.class);
+            this.startActivityForResult(i, REQUEST_CODE_ADD_ITEMS);
+    }
+	
+	
+	
+	
+    
+	public void onBackClick(View view) {
+		Intent i = new Intent(this, Lista1.class);
+		this.startActivityForResult(i, REQUEST_CODE_UPDATE_ITEMS);
+ 
+		
+	}	
+	
+		public void onDeleteButtonClicked(View view) {
+	
+		Intent j = this.getIntent();
+		Molist3 ls = (Molist3) j.getSerializableExtra(Lista2.EXTRA_ITEMS);
+		this.ItemsToUpdate = ls;
+		
+		
+		Molist3 it = dslist3.deleteList3total(this.ItemsToUpdate);
+		Person p = ds.deletePerson(this.personToUpdate);
+		
+		Intent i = new Intent();
+		i.putExtra(Lista1.EXTRA_PERSON, p);
+		i.putExtra(Lista1.EXTRA_REMOVE, true);
+		i.putExtra(List3.EXTRA_ITEMS, it);
+	    i.putExtra(List3.EXTRA_REMOVE_ITEMS, true);
+		this.setResult(RESULT_OK, i);
+		
+		Intent k = new Intent(this, Lista1.class);
+		
+		this.finish();
+		this.startActivity(k);
+        
+
+	}
+		
+		public void emptylist(View view){
+			
+			Intent j = this.getIntent();
+			Molist3 ls = (Molist3) j.getSerializableExtra(Lista2.EXTRA_ITEMS);
+			Person p = (Person) j.getSerializableExtra(Lista1.EXTRA_PERSON);
+			this.ItemsToUpdate = ls;
+			
+			
+			Molist3 it = dslist3.deleteList3total(this.ItemsToUpdate);
+			
+			
+			Intent i = new Intent();
+			;
+			i.putExtra(List3.EXTRA_ITEMS, it);
+		    i.putExtra(List3.EXTRA_REMOVE_ITEMS, true);
+			this.setResult(RESULT_OK, i);
+			
+			Intent k = new Intent(this, List3.class);
+			
+			
+			
+			
+			this.finish();
+			k.putExtra("person", p);
+			
+			
+			
+			this.finish();
+			this.startActivityForResult(k, Lista1.REQUEST_CODE_UPDATE_PERSON);
+		}
+		
+
+
+
+	
+	@Override
+	protected void onResume() {
+		ds.open();
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		ds.close();
+		super.onPause();
+	}
+	
+
+}
+
+
+	
